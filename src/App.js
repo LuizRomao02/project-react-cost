a0d18f3f222a3bfe4e33125decf87721205d2d3c
LuizRomao02 <luizromao7@outlook.com>
2022-03-17 12:10:19 -0300

    forms - api json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
@@ -14,18 +14,18 @@ function App() {
   return (
     <Router>
       <div>
-       <Navbar/>
+        <Navbar />
       </div>
       <Container customClass="min-height">
         <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
